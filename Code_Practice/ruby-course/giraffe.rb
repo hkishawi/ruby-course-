3f1e07566a8f4094351ba5c36bc5ea575b73ac54
Hanny Kishawi <hkishawi@gmail.com>
2020-12-03 15:56:47 -0500

    array basics

--- Code_Practice/ruby-course/giraffe.rb
+++ Code_Practice/ruby-course/giraffe.rb
@@ -1,12 +1,3 @@
 
-
-puts "Enter a color: "
-color = gets.chomp()
-puts "Enter a noun: "
-plural_noun = gets.chomp()
-puts "Enter a celebrity: "
-celebrity = gets.chomp()
-
-puts ("Roses are " + color)
-puts (plural_noun + " are blue")
-puts ("I love " + celebrity)
+friends = Array["Kevin", "Mike", "Sharon", "Suzana"]
+puts friends.sort()
