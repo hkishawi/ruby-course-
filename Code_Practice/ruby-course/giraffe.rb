85dc50f254d9a59c27c26b103deb1f6ac4d25d26
Hanny Kishawi <hkishawi@gmail.com>
2020-12-03 17:44:09 -0500

    while basics

--- Code_Practice/ruby-course/giraffe.rb
+++ Code_Practice/ruby-course/giraffe.rb
@@ -1,27 +1,5 @@
-
-def get_day_name(day)
-  day_name = ""
-
-  case day
-  when "mon"
-    day_name = "Monday"
-  when "tue"
-    day_name = "Tuesday"
-  when "wed"
-    day_name = "Wednesday"
-  when "thu"
-    day_name = "Thursday"
-  when "fri"
-    day_name = "Friday"
-  when "sat"
-    day_name = "Saturday"
-  when "sun"
-    day_name = "Sunday"
-  else
-    day_name = "Invalid abbreviation, BRO"
-  end
-
-return day_name
+index = 1
+while index <= 8
+  puts index
+  index = index += 1
 end
-
-puts get_day_name("cat")
