487e650b772226e008877c67c06aa0a278401c2a
Hanny Kishawi <hkishawi@gmail.com>
2020-12-03 17:15:44 -0500

    a better calc

--- Code_Practice/ruby-course/giraffe.rb
+++ Code_Practice/ruby-course/giraffe.rb
@@ -1,12 +1,21 @@
 
-def max(num1, num2, num3)
-  if num1 >= num2 and num1 >= num3
-    return num1
-  elsif num2 >= num1 and num2 >= num3
-    return num2
-  else
-    return num3
-  end
-end
+puts "Enter first number: "
+num1 = gets.chomp().to_f
+puts "Enter operator: "
+op = gets.chomp()
+puts "Enter 2nd number"
+num2 = gets.chomp().to_f
 
-puts max(1, 20, 4)
+if op == "+"
+  puts (num1 + num2)
+elsif op == "-"
+  puts (num1 - num2)
+elsif op == "*"
+  puts (num1 * num2)
+elsif op == "/"
+  puts (num1 / num2)
+elsif op == "%"
+  puts (num1 % num2)
+else
+  puts "Invalid operator"
+end
