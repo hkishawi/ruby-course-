c635f0e31bfc0110d1765754cc8ae420759539a6
Hanny Kishawi <hkishawi@gmail.com>
2020-12-03 18:14:22 -0500

    for loops

--- Code_Practice/ruby-course/giraffe.rb
+++ Code_Practice/ruby-course/giraffe.rb
@@ -1,33 +1,10 @@
 
-secret_word = "giraffe"
-guess = ""
-guess_count = 0
-guess_limit = 3
-out_of_guesses = false
+friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
 
-# def counter
-#  if guess_count != 5
-#    guess_count += 1
-#   else
-#    puts "You LOST"
-#  end
-# end
-
-while guess != secret_word and !out_of_guesses
-  if guess_count < guess_limit
-    puts "Enter guess: "
-    guess = gets.chomp()
-    guess_count += 1
-    puts guess_count
-  else
-    out_of_guesses = true
-  end
+for index in 0..5
+  puts index
 end
 
-if out_of_guesses
-  puts "You lose"
-  puts guess_count
-else
-  puts "You won!"
-  puts guess_count 
+6.times do |index|
+  puts index
 end
