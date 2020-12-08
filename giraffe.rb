67b81a9756e7ece0b175c4479c9173f650576872
Hanny Kishawi <hkishawi@gmail.com>
2020-12-08 17:41:57 -0500

    using modules and tools

--- giraffe.rb
+++ giraffe.rb
@@ -1,33 +1,5 @@
 
-class Chef   #superclass
-  def make_chicken
-    puts "The chef makes chicken"
-  end
+require_relative "useful_tools.rb"
+include Tools
 
-  def make_salad
-    puts "the chef makes salad"
-  end
-
-  def make_special_dish
-    puts "the chef makes bbq rib"
-  end
-
-end 
-
-class ItalianChef < Chef #italian inherits superclass
-  def make_special_dish
-    puts "the Italian chef makes escargogi"
-  end 
-
-  def make_pasta
-    puts "the Italian chef makes pasta"
-  end 
-end 
-
-
-chef = Chef.new()
-chef.make_chicken
-
-Italian_chef = ItalianChef.new()
-Italian_chef.make_salad
-Italian_chef.make_pasta
+Tools.sayhi("Hanny")
