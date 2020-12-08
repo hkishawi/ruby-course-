39343abe9b48a4ea45b1a28d9e86b954c4c96920
Hanny Kishawi <hkishawi@gmail.com>
2020-12-08 16:35:10 -0500

    using class to call objects

--- giraffe.rb
+++ giraffe.rb
@@ -7,20 +7,15 @@ class Book
     @author = author
     @pages = pages
 
-    puts("hello " + name)
+    puts("hello " + author)
   end
 end 
 
-book1 = Book.new("Hagrid")
-book1.title = "Harry Potter"
-book1.author = "JK Rowling"
-book1.pages = 400
+book1 = Book.new("Hagrid", "JK Rowling", 99)
 
-book2 = Book.new("Gandlaf")
-book2.title = "LOR"
-book2.author = "Tolkein"
-book2.pages = 500
+book2 = Book.new("Gandlaf", "JRR", 21)
 
-puts book1.author
-puts book2.pages
+
+
+
 
