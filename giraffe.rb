30861810eeba55e1b7bca0fd292f0d3881181215
Hanny Kishawi <hkishawi@gmail.com>
2020-12-08 16:54:55 -0500

    object methods

--- giraffe.rb
+++ giraffe.rb
@@ -1,21 +1,24 @@
 
-class Book 
-  attr_accessor :title, :author, :pages
-  def initialize(title, author, pages)
-    @title = title 
-    # refers to title object that was passed in
-    @author = author
-    @pages = pages
+class Student
+  attr_accessor :name, :major, :gpa
+  def initialize(name, major, gpa)
+    @name = name
+    @major = major
+    @gpa = gpa
+  end 
 
-    puts("hello " + author)
+  def has_honors()
+    if @gpa >= 3.5
+      return true
+    end
+    return false 
   end
 end 
 
-book1 = Book.new("Hagrid", "JK Rowling", 99)
-
-book2 = Book.new("Gandlaf", "JRR", 21)
 
 
 
+student1 = Student.new("Jim", "Business", 2.6)
+student2 = Student.new("Pam", "Art", 3.6)
 
-
+puts student2.has_honors
