a0b99a7c35c363cd498cbc594183f52ce0d4c7f4
Hanny Kishawi <hkishawi@gmail.com>
2020-12-08 16:31:31 -0500

    added book attr. to class

--- giraffe.rb
+++ giraffe.rb
@@ -1,17 +1,26 @@
 
 class Book 
   attr_accessor :title, :author, :pages
+  def initialize(title, author, pages)
+    @title = title 
+    # refers to title object that was passed in
+    @author = author
+    @pages = pages
+
+    puts("hello " + name)
+  end
 end 
 
-book1 = Book.new()
+book1 = Book.new("Hagrid")
 book1.title = "Harry Potter"
 book1.author = "JK Rowling"
 book1.pages = 400
 
-book2 = Book.new()
+book2 = Book.new("Gandlaf")
 book2.title = "LOR"
 book2.author = "Tolkein"
 book2.pages = 500
 
 puts book1.author
 puts book2.pages
+
