0c3e58e9001952a5e6af1903cf31bb9269e490bd
Hanny Kishawi <hkishawi@gmail.com>
2020-12-08 16:19:04 -0500

    classes and objs

--- giraffe.rb
+++ giraffe.rb
@@ -1,11 +1,17 @@
-lucky_nums = [4, 8, 15, 16, 23, 42]
 
-lucky_nums[4]
+class Book 
+  attr_accessor :title, :author, :pages
+end 
 
-begin
-  lucky-nums["dog"]
-rescue ZeroDivisionError
-  puts "Division by zero error"
-rescue TypeError => e
-  puts e
-end
+book1 = Book.new()
+book1.title = "Harry Potter"
+book1.author = "JK Rowling"
+book1.pages = 400
+
+book2 = Book.new()
+book2.title = "LOR"
+book2.author = "Tolkein"
+book2.pages = 500
+
+puts book1.author
+puts book2.pages
